64d38b9876bf231bfa3f778e709a229cd7061f87
gitprovatore <d661185@rtrtr.com>
2012-06-16 13:11:51 +0200

    updated everything

---
+++ README.md
@@ -0,0 +1,2 @@
+##hey what r u doin' here??
+move on there's nothing for you!
---
+++ testificate.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+class Veritius
+    def helloworld( name )
+        puts "hello #{name}"
+    end
+end
+
+objectius = Veritius.new
+objectius.helloworld('francesco')
